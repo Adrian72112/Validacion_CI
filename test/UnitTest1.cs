86bcb1ed6c68eff13deb6204a888aa8d9360c4cb
Adrian Tesore <adrian72112@gmail.com>
2018-09-03 19:15:15 -0300

    Ahora si corre y anda todo

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace Person.Test
         public void ValidarCIFalsa()
         {
             Persona persona = new Persona("John Doe", "1.234.567-8");
-            Boolean actual = persona.validarFormatoCI(persona.numero("4.870.499-7"));
+            Boolean actual = persona.validarFormatoCI(persona.numero("1.870.499-7"));
             Assert.False(actual,"Correcto");
         }
         [Fact]
