51c627a811b7c5c8e303c27165a5af739529d6ab
Adrian Tesore <adrian72112@gmail.com>
2018-09-03 19:25:48 -0300

    Fact to theory xunit test

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -5,19 +5,15 @@ namespace Person.Test
 {
     public class UnitTest1
     {
-        [Fact]
-        public void ValidarCIFalsa()
+        [Theory]
+        [InlineData("4.870.499-7")]
+        [InlineData("1.234.567-8")]
+        [InlineData("3.742.388-1")]
+        public void ValidarCI(string value)
         {
-            Persona persona = new Persona("John Doe", "1.234.567-8");
-            Boolean actual = persona.validarFormatoCI(persona.numero("1.870.499-7"));
-            Assert.False(actual,"Correcto");
-        }
-        [Fact]
-        public void ValidarCIVerdadero()
-        {
-            Persona persona = new Persona("John Doe", "4-870-499-7");
-            Boolean actual = persona.validarFormatoCI(persona.numero("4.870.499-7"));
-            Assert.True(actual);
+            Persona persona = new Persona("John Doe", value);
+            Boolean actual = persona.validarFormatoCI(persona.numero(value));
+            Assert.True(actual,$"{value} no es una cedula valida");
         }
     }
 }
