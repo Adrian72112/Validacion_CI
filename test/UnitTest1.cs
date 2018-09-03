76025007b56d94b15e60f8d521226edebacf1898
Adrian Tesore <adrian72112@gmail.com>
2018-09-03 19:33:52 -0300

    commit

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -5,13 +5,22 @@ namespace Person.Test
 {
     public class UnitTest1
     {
+
+        [Fact]
+        public void ValidarNombre()
+        {
+            const String nombrePrueba = "Adrian Tesore";
+            Persona person = new Persona(nombrePrueba, "48704997");
+            Assert.Equal(nombrePrueba,person.Nombre);
+        }
+
         [Theory]
         [InlineData("4.870.499-7")]
         [InlineData("1.234.567-8")]
         [InlineData("3.742.388-1")]
         public void ValidarCI(string value)
         {
-            Persona persona = new Persona("John Doe", value);
+            Persona persona = new Persona("Adrian Tesore", value);
             Boolean actual = persona.validarFormatoCI(persona.numero(value));
             Assert.True(actual,$"{value} no es una cedula valida");
         }
